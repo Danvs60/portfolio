4af8cde819f0bbd0eaff2a429c897f0a93aeb679
D.Bartolini <db666@kent.ac.uk>
2021-02-18 16:42:07 +0000

    removed unnecessary code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,16 +11,16 @@ export default function Projects() {
       <div className="absolute bottom-0 w-full">
         <BottomMountains />
       </div>
-      <div className="flex flex-col px-32">
+      <div className="flex flex-col justify-between flex-grow px-32 py-16">
         <section className="flex flex-col gap-16 z-10">
           <h1 className="font-bold text-8xl z-10">Projects</h1>
           <div className="flex gap-x-12 cards">
             <ProjectCard />
-            <ProjectCard delay="1" />
-            <ProjectCard delay="1.5" />
+            <ProjectCard />
+            <ProjectCard />
           </div>
         </section>
-        <p className="text-5xl">
+        <p className="text-5xl z-10">
           Find more on my <span className="underline">GitHub</span>
         </p>
       </div>
