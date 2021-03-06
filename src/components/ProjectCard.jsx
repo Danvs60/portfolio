b84484be010faabbf5691d699c1c2a4854ea2e02
D.Bartolini <db666@kent.ac.uk>
2021-03-06 02:29:42 +0000

    mobile view projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ProjectCard({ title, descr, img, className, url }) {
   return (
     <div
-      className={`h-72 px-8 py-8 card ${className} overflow-hidden rounded-2xl shadow-lg bg-mintCream`}
+      className={`h-72 w-full  xl:w-1/4 px-8 py-8 card ${className} overflow-hidden rounded-2xl shadow-lg bg-mintCream`}
     >
       <a href={url}>
         <div className=" cursor-pointer scale-95 transition duration-200 ease-in-out transform hover:scale-100 relative flex flex-col gap-y-4 text-lg text-gray-900">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import waves from "../images/waves.svg";
+import tablet_waves from "../images/tablet_waves.svg";
 import shooting from "../images/shooting.svg";
 import ga from "../images/undraw_problem_solving.svg";
 import lofi from "../images/undraw_experience_design.svg";
@@ -10,15 +11,26 @@ export default function Projects() {
   return (
     <>
       <img
-        className="absolute right-36 top-28 stars"
+        className="fixed right-36 top-28 stars"
         src={shooting}
         alt="shootingStars"
       />
-      <img className="absolute bottom-0" src={waves} alt="waves" />
-      <div className="flex flex-col justify-between flex-grow px-32 pb-12">
-        <section className="flex flex-col gap-24 z-10">
-          <h1 className="font-bold text-8xl z-10">Some of my work</h1>
-          <div className="flex gap-x-16 cards">
+      <img
+        className="fixed bottom-0 w-full md:hidden"
+        src={tablet_waves}
+        alt="tablet"
+      />
+      <img
+        className="fixed bottom-0 w-full hidden md:block"
+        src={waves}
+        alt="waves"
+      />
+      <div className="flex flex-col justify-between flex-grow px-8 md:px-32 pb-12">
+        <section className="flex flex-col gap-y-12 lg:gap-y-24 z-10">
+          <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl z-10">
+            Some of my work
+          </h1>
+          <div className="w-full pb-12 flex flex-col lg:justify-start lg:flex-row flex-wrap gap-16 cards">
             <ProjectCard
               title="Genetic Algorithms"
               descr="2 algorithms that will pick the solution with the best fitness."
@@ -29,19 +41,19 @@ export default function Projects() {
               title="Lo-Fi Prototype"
               descr="Lo-Fi prototype of a touchscreen app that lets users order their coffee at the cafe."
               img={lofi}
-              className=" mt-32"
+              className="xl:mt-32"
               url="https://github.com/Danvs60/lofi-prototyping"
             />
             <ProjectCard
               title="Tic Tac Toe"
               descr="The good old tic tac toe game, built in html and javascript."
               img={ttt}
-              className=" mt-64"
+              className="xl:mt-64"
               url="https://github.com/Danvs60/tictactoe"
             />
           </div>
         </section>
-        <p className="text-5xl z-10 projects">
+        <p className=" text-3xl md:text-4xl lg:text-5xl z-10 projects">
           Find more on my{" "}
           <a
             href="https://github.com/Danvs60"
